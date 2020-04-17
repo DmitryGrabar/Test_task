ac71e601403478b456dd9aba143e6db5980bd721
Dzmitry Hrabar <dzmitry_hrabar@epam.com>
2020-04-17 10:58:30 +0300

    TableTime - added equals & hashcode BusServiceImpl - totally changed getEffectiveBuses function logic BussServiceImplTest - added customer test

--- src/main/java/com/github/grabar/bus_task/service/impl/BusServiceImpl.java
+++ src/main/java/com/github/grabar/bus_task/service/impl/BusServiceImpl.java
@@ -5,10 +5,8 @@ import com.github.grabar.bus_task.entity.CompanyName;
 import com.github.grabar.bus_task.entity.TimeTable;
 import com.github.grabar.bus_task.service.BusService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalTime;
+import java.util.*;
 
 import static java.time.temporal.ChronoUnit.MINUTES;
 
@@ -17,17 +15,35 @@ public class BusServiceImpl implements BusService {
 
     @Override
     public List<Bus> getEffectiveBuses(List<Bus> buses) {
-        Map<Long, Bus> busesWithDriveTime = new HashMap<>();
-
-        for (Bus bus : buses) {
+        LinkedList<Bus> tempList = new LinkedList<>(buses);
+        ListIterator<Bus> iterator = tempList.listIterator();
+        while (iterator.hasNext()) {
+            Bus bus = iterator.next();
             long timeDiff = getTimeDiff(bus.getTimeTable());
             if (timeDiff > 0 && timeDiff < EFFECTIVE_TIME) {
-                if (!busesWithDriveTime.containsKey(timeDiff) || bus.getCompanyName().equals(CompanyName.POSH)) {
-                    busesWithDriveTime.put(timeDiff, bus);
+                LocalTime depTime = bus.getTimeTable().getDeparture();
+                LocalTime arrTime = bus.getTimeTable().getArrival();
+                for (Bus secondBus : tempList) {
+                    if (bus.equals(secondBus)) {
+                        continue;
+                    } else if (bus.getTimeTable().equals(secondBus.getTimeTable()) && !bus.getCompanyName().equals(secondBus.getCompanyName())) {
+                        if (bus.getCompanyName().equals(CompanyName.GROTTY)) {
+                            iterator.remove();
+                        }
+                        break;
+                    }
+                    boolean isDepartureTimeBefore = !depTime.isAfter(secondBus.getTimeTable().getDeparture());
+                    boolean isArrivalTimeAfter = !arrTime.isBefore(secondBus.getTimeTable().getArrival());
+                    if (isDepartureTimeBefore && isArrivalTimeAfter) {
+                        iterator.remove();
+                        break;
+                    }
                 }
+            } else {
+                iterator.remove();
             }
         }
-        return new ArrayList<>(busesWithDriveTime.values());
+        return new ArrayList<>(tempList);
     }
 
     @Override
